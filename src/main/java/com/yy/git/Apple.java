4ad5a57e72e5ca165257de6760dfff7412d93801
tom_glb <eclipse_user@qq.com>
2020-01-18 17:55:57 +0800

    this is hotfix first

--- src/main/java/com/yy/git/Apple.java
+++ src/main/java/com/yy/git/Apple.java
@@ -4,6 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("hello.shit");
 		System.out.println("hello.double shit");
-		
+		System.out.println("hello.i am hot_fix");
 	}
 }
