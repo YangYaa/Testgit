10d3a95f2084a9b1ee18dd483522e8b31ddbd20b
tom_glb <eclipse_user@qq.com>
2020-01-18 18:21:33 +0800

    this is yy second commit hot_fix

--- src/main/java/com/yy/git/Happy.java
+++ src/main/java/com/yy/git/Happy.java
@@ -1,5 +1,9 @@
 package com.yy.git;
 
 public class Happy {
-
-}
+	public static void main(String[] args) {
+		System.out.println("hello.shit");
+		System.out.println("hello.double shit");
+		System.out.println("hello.i am hot_fix second");
+	}
+}	
